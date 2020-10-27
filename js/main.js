1f0e1f54db23f6a71921d959065cd23001b9c99a
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-26 18:05:31 -0700

    Listen for 'submit' events on the form. #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var $avatarUrlInput = document.querySelector('.avatarUrl');
 var $profileImage = document.querySelector('.placeholder');
+var $save = document.querySelector('button');
 
 function updateProfileImage(event) {
   $profileImage.setAttribute('src', event.target.value);
 }
+
+function formSubmitted(event) {
+
+}
 $avatarUrlInput.addEventListener('input', updateProfileImage);
+
+$save.addEventListener('submit', formSubmitted);
