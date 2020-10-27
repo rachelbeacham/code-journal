d958677edae11a708dd3e1ebb64fab4eb8a1ae57
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-27 11:04:47 -0700

    Assign all of the form's values into the profile data model when the form is submitted. Reset the form inputs when the form is submitted.Make sure that the user's data is persisted to local storage between reloads. #1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 var $avatarUrl = document.querySelector('#avatarUrl');
 var $profileImage = document.querySelector('.placeholder');
 var $save = document.querySelector('button');
-var $username = document.querySelector('#username')
-var $fullName = document.querySelector('#fullName')
-var $location = document.querySelector('#location')
-var $bio = document.querySelector('#bio')
-var $form = document.querySelectorAll('form');
-
+var $username = document.querySelector('#username');
+var $fullName = document.querySelector('#fullName');
+var $location = document.querySelector('#location');
+var $bio = document.querySelector('#bio');
 
 function updateProfileImage(event) {
   $profileImage.setAttribute('src', event.target.value);
 }
 
 function formSubmitted(event) {
-   data.profile.username = $username;
-   data.profile.fullName = $fullName;
-   data.profile.location = $location;
-   data.profile.bio = $bio;
-   data.profile.avatarUrl = $avatarUrl;
+  data.profile.username = $username.value;
+  data.profile.fullName = $fullName.value;
+  data.profile.location = $location.value;
+  data.profile.bio = $bio.value;
+  data.profile.avatarUrl = $avatarUrl.value;
+  window.addEventListener('beforeunload', function () {
+    var dataProfileJson = JSON.stringify(data.profile);
+    localStorage.setItem('profile', dataProfileJson);
+  });
 }
 
 $avatarUrl.addEventListener('input', updateProfileImage);
-$save.addEventListener('submit', formSubmitted);
+
+$save.addEventListener('click', formSubmitted);
