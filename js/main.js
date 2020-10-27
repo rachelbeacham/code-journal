fcda99949d9b366812e5cad43a288b7651e79c14
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-26 20:19:46 -0700

    Assign all of the form's values into the profile data model when the form is submitted. #1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-var $avatarUrlInput = document.querySelector('.avatarUrl');
+var $avatarUrl = document.querySelector('#avatarUrl');
 var $profileImage = document.querySelector('.placeholder');
 var $save = document.querySelector('button');
+var $username = document.querySelector('#username')
+var $fullName = document.querySelector('#fullName')
+var $location = document.querySelector('#location')
+var $bio = document.querySelector('#bio')
+var $form = document.querySelectorAll('form');
+
 
 function updateProfileImage(event) {
   $profileImage.setAttribute('src', event.target.value);
 }
 
 function formSubmitted(event) {
-
+   data.profile.username = $username;
+   data.profile.fullName = $fullName;
+   data.profile.location = $location;
+   data.profile.bio = $bio;
+   data.profile.avatarUrl = $avatarUrl;
 }
-$avatarUrlInput.addEventListener('input', updateProfileImage);
 
+$avatarUrl.addEventListener('input', updateProfileImage);
 $save.addEventListener('submit', formSubmitted);
