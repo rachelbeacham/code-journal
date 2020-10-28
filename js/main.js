2681837eaa42afc74f58b56c173f9f4317fb2a6c
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-27 21:45:04 -0700

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML. #2

--- js/main.js
+++ js/main.js
@@ -28,3 +28,68 @@ window.addEventListener('beforeunload', function () {
   var dataProfileJson = JSON.stringify(data.profile);
   localStorage.setItem('profile', dataProfileJson);
 });
+
+function renderProfile (data) {
+  var containerDiv = document.createElement('div');
+  containerDiv.setAttribute('data-view', 'profile');
+  containerDiv.className = 'container';
+
+  var nameSection = document.createElement('section');
+  nameSection.className = 'row';
+
+  var name = document.createElement('h1');
+  name.className = 'column-full';
+  name.textContent = data.profile.fullName;
+
+  var profileSection = document.createElement('section');
+  profileSection.className = 'row';
+
+  var imageDiv = document.createElement('div');
+  imageDiv.className = 'column-half';
+
+
+  var profileImage = document.createElement('img');
+  profileImage.setAttribute('src', data.profile.avatarUrl);
+  profileImage.setAttribute('alt', 'user profile photo');
+
+  var profileInfoDiv = document.createElement('div');
+  profileInfoDiv.className = 'column-half profile-info';
+
+  var usernameDiv = document.createElement('div');
+
+  var userIcon = document.createElement('i');
+  userIcon.className = 'fas fa-user';
+
+  var profileUsername = document.createElement('span');
+  profileUsername.className = 'username';
+  profileUsername.textContent = data.profile.username;
+
+  var locationDiv = document.createElement('div');
+
+  var locationIcon = document.createElement('i');
+  locationIcon.className = 'fas fa-map-marker-alt';
+
+  var profileLocation = document.createElement('span');
+  profileLocation.className = 'location';
+  profileLocation.textContent = data.profile.location;
+
+  var profileBio = document.createElement('p');
+  profileBio.className = 'bio';
+  profileBio.textContent = data.profile.bio;
+
+  containerDiv.appendChild(nameSection);
+  containerDiv.appendChild(profileSection);
+  nameSection.appendChild(name);
+  profileSection.appendChild(imageDiv);
+  profileSection.appendChild(profileInfoDiv);
+  imageDiv.appendChild(profileImage);
+  profileInfoDiv.appendChild(usernameDiv);
+  profileInfoDiv.appendChild(locationDiv);
+  profileInfoDiv.appendChild(profileBio);
+  usernameDiv.appendChild(userIcon);
+  usernameDiv.appendChild(profileUsername);
+  locationDiv.appendChild(locationIcon);
+  locationDiv.appendChild(profileLocation);
+
+  return containerDiv;
+}
