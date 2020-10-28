5a3e797d2913e998b673d0a45598bd38e433e4b8
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-27 17:01:52 -0700

    Changes appled. #1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
 var $avatarUrl = document.querySelector('#avatarUrl');
 var $profileImage = document.querySelector('.placeholder');
-var $save = document.querySelector('button');
 var $username = document.querySelector('#username');
 var $fullName = document.querySelector('#fullName');
 var $location = document.querySelector('#location');
 var $bio = document.querySelector('#bio');
+var $form = document.querySelector('.edit-profile-form');
 
 function updateProfileImage(event) {
   $profileImage.setAttribute('src', event.target.value);
 }
 
 function formSubmitted(event) {
+  event.preventDefault();
   data.profile.username = $username.value;
   data.profile.fullName = $fullName.value;
   data.profile.location = $location.value;
   data.profile.bio = $bio.value;
   data.profile.avatarUrl = $avatarUrl.value;
-  window.addEventListener('beforeunload', function () {
-    var dataProfileJson = JSON.stringify(data.profile);
-    localStorage.setItem('profile', dataProfileJson);
-  });
+  $form.reset();
 }
 
 $avatarUrl.addEventListener('input', updateProfileImage);
 
-$save.addEventListener('click', formSubmitted);
+$form.addEventListener('submit', formSubmitted);
+
+window.addEventListener('beforeunload', function () {
+  var dataProfileJson = JSON.stringify(data.profile);
+  localStorage.setItem('profile', dataProfileJson);
+});
